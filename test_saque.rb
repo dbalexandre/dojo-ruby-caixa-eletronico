a046a501415d158ba09419dd154c92365322d13e
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2011-06-17 08:57:08 -0300

    first commit

--- test_saque.rb
+++
@@ -1,9 +0,0 @@
-require 'test/unit'
-require_relative 'saque'
-
-class TestSaque < Test::Unit::TestCase
-  
-  def test_saque_dez_reais
-    assert_equal(@caixa_eletronico.saque(10), 10)
-  end
-end
